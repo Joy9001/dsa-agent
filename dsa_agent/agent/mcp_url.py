c132cbc620641f289b4406d8d422fcd7b0bc331c
joymridha939 <joymridha939@gmail.com>
2025-08-13 13:08:54 +0000

    chore: remove caching from get_smithery_url

--- dsa_agent/agent/mcp_url.py
+++ dsa_agent/agent/mcp_url.py
@@ -1,10 +1,9 @@
 import base64
 import json
-from functools import lru_cache
+
 import config as cfg
 
 
-@lru_cache(maxsize=128)
 def get_smithery_url(base_url: str, config: dict, api_key: str, profile: str):
     config_b64 = base64.b64encode(json.dumps(config).encode()).decode()
     url = f"{base_url}?config={config_b64}&api_key={api_key}&profile={profile}"
