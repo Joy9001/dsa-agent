b2081238d29142e59ad18ebf3580f9a48e340e68
Joy Mridha <joymridha939@gmail.com>
2025-08-22 09:48:47 +0000

    fix(config): clean up configuration variables

--- dsa_agent/config.py
+++ dsa_agent/config.py
@@ -1,5 +1,6 @@
 from decouple import config
 
+LOG_LEVEL = config("LOG_LEVEL", default="INFO")
 PG_CONN_STR = config("PG_CONN_STR")
 
 GEMINI_API_KEY = config("GEMINI_API_KEY")
@@ -8,13 +9,11 @@ LC_MCP_BASE_URL = config(
     "LC_MCP_BASE_URL",
     default="https://server.smithery.ai/@jinzcdev/leetcode-mcp-server/mcp",
 )
-LC_SITE = config("LC_SITE", default="global")
-LC_SESSION = config("LC_SESSION")
-SMITHERY_API_KEY = config("SMITHERY_API_KEY")
-SMITHERY_PROFILE = config("SMITHERY_PROFILE")
 GH_MCP_BASE_URL = config(
     "GH_MCP_BASE_URL", default="https://server.smithery.ai/@smithery-ai/github/mcp"
 )
-GH_TOKEN = config("GH_TOKEN")
 
-LOG_LEVEL = config("LOG_LEVEL", default="INFO")
+SMITHERY_API_KEY = config("SMITHERY_API_KEY")
+SMITHERY_PROFILE = config("SMITHERY_PROFILE")
+
+
